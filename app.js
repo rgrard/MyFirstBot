830725258d9f1d995862b19d822d79bd7735a6a8
Romain GRARD <romain.grard@ineat.ca>
2018-05-25 16:30:44 -0400

    Looking for interactions

--- app.js
+++ app.js
@@ -17,9 +17,12 @@ server.listen(process.env.PORT || 3000, function()
 var connector = new builder.ChatConnector
 ({ appId: '554e6f88-e090-4cb2-a020-41fbc1535a33', appPassword: 'vuegGTE102([fesMGDV75[?' }); 
 var bot = new builder.UniversalBot(connector);
+
 server.post('/api/messages', connector.listen());
 
 // Create bot dialogs
 bot.dialog('/', function (session) {
-    session.send("Bonjour, je suis consultant3000!");
+    session.send("Bonjour, je suis consultant3000! \n J'ai été créé pour concevoir des phrases comme un consultant pourrait les formuler afin de ne jamais avoir le syndrome de la page blanche lors d'un audit!");
+    
+    //session.send("2");
 });
